ef7488992b4947d36000b9ed2b49b1f30e69644b
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-10-02 21:30:26 +0100

    Refactor: Remove project button and enhance navigation to project section - Remove the project button from the main interface. - Add functionality to navigate directly to the project section when project is clicked in the navbar. - Adjust projects style.

--- script.js
+++ script.js
@@ -1,68 +1,66 @@
+// Grabbing DOM elements for navigation and content
+const navLinks = document.querySelectorAll(".navbar");
+const mainPage = document.querySelector('.mainPage');
 const contents = document.querySelectorAll('.content');
-const learning = document.getElementById('learning');
-
 const contactForm = document.getElementById("contactForm");
 const thankyouMessage = document.getElementById("thankyouMessage");
 
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to hide all the sections
+    function hideAllContents() {
+        contents.forEach(content => content.style.display = 'none');
+    }
 
+    // Event listener for the navigation links
+    navLinks.forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
 
-// Function to toggle content based on menu clicks
-function toggleContent(contentId) {
-    // Hide all content sections
-    contents.forEach(function (content) {
-        
-        content.style.display = 'none';
+            // Determine which link was clicked
+            const linkTarget = e.currentTarget.getAttribute("data-link");
+
+            // If the Projects link is clicked, hide the main content, else show it
+            if (linkTarget === 'projects') {
+                mainPage.style.display = 'none';
+            } else {
+                mainPage.style.display = 'block';
+            }
+
+            // Hide all the sections
+            hideAllContents();
+
+            // Display the clicked section
+            document.getElementById(linkTarget).style.removeProperty('display');
+        });
     });
 
-    // Show the selected content
-    document.getElementById(contentId).style.display = 'block';
-}
-
-// Add event listeners for menu clicks
-const links = Array.from(document.getElementsByClassName('navbar'));
-
-links.forEach((link) => {
-    link.addEventListener('click', function () {
-        toggleContent(link.dataset.link);
-    });
+    // Initial state setup when the page loads
+    hideAllContents();
+    mainPage.style.display = 'block';
+    document.getElementById('statement').style.display = 'block';
 });
 
-// Function to toggle project card visibility
-function toggleScrollProjectCards() {
-    // Toggle the visibility of project cards
-    learning.classList.toggle('show-projects');
-    learning.classList.toggle('hidden');
-    learning.scrollIntoView({ behavior: 'smooth', block: 'start' });
-}
-
-// Add event listener to the "Click Here" button for toggling project cards
-document.getElementById('clickBtn').addEventListener('click', () => {
-    toggleScrollProjectCards(); // Toggle project card visibility after scrolling
-});
-
-// Initial content to display
-toggleContent('statement');
-
-
-
-// Function to show/hide 'thank you message'
+// Function to display the 'thank you' message
 function showThankyouMessage() {
     thankyouMessage.classList.add("visible");
 }
 
+// Function to hide the 'thank you' message
 function hideThankyouMessage() {
     thankyouMessage.classList.remove("visible");
 }
 
-// Contact form
-function registerEmail(event) {
+// Event listener for the contact form submission
+contactForm.addEventListener("submit", function(event) {
     event.preventDefault();
 
+    // Extract data from the form
     const name = contactForm.querySelector('input[name="name"]').value;
     const email = contactForm.querySelector('input[name="email"]').value;
     const subject = contactForm.querySelector('input[name="subject"]').value;
     const message = contactForm.querySelector('textarea[name="message"]').value;
 
+    // AJAX call to send the form data
     fetch("https://formspree.io/f/xaygovyn", {
         method: "POST",
         headers: {
@@ -76,20 +74,22 @@ function registerEmail(event) {
             message,
         }),
     })
-        .then((response) => {
-            if (response.ok) {
-                contactForm.reset();
-                showThankyouMessage();
-                setTimeout(hideThankyouMessage, 3000);
-            } else {
-                console.error("Failed to send email.");
-            }
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-        });
-}
-
-contactForm.addEventListener("submit", registerEmail);
+    .then((response) => {
+        // Check for a successful response, reset the form and display a 'thank you' message
+        if (response.ok) {
+            contactForm.reset();
+            showThankyouMessage();
+            setTimeout(hideThankyouMessage, 3000);
+        } else {
+            // Log an error for failed email submission
+            console.error("Failed to send email.");
+            alert("There was an issue sending your message. Please try again later.");
+        }
+    })
+    .catch((error) => {
+        // Log any fetch errors
+        console.error("Error:", error);
+    });
+});
 
 
