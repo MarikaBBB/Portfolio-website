e0580595b20f7d67c76a28b0a415d823f01855c5
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-10-11 00:29:08 +0100

    Refactor hamburger menu handling for improved UX

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,28 +1,12 @@
 /**
  * Hamburger Menu handling
  */
-const btn = document.getElementById("yourButtonId");
-const modal = document.getElementById("yourModalId");
-const span = document.querySelector(".closeButtonClass"); 
-const navLinks = document.querySelectorAll(".menuBar a");
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const nav = document.getElementById("nav-menu");
+const hamburgerIcon = document.querySelector(".mobile-menu");
 
-navLinks.forEach(link => {
-    link.addEventListener("click", function() {
-        // Close the modal
-        modal.style.display = "none";
-        
-        // Close the mobile menu
-        const nav = document.getElementById("nav-menu");
-        nav.classList.remove("mobile-active");
-    });
-});
-
-document.getElementById("hamburger-btn").addEventListener("click", toggleMenu);
-
+// Function to toggle the mobile menu
 function toggleMenu() {
-    const nav = document.getElementById("nav-menu");
-    const hamburgerIcon = document.querySelector(".mobile-menu");
-
     nav.classList.toggle("mobile-active");
 
     // Toggle the hamburger icon
@@ -33,26 +17,51 @@ function toggleMenu() {
     }
 }
 
-window.addEventListener("resize", function () {
-    const nav = document.getElementById("nav-menu");
+// Event listener for hamburger button click
+hamburgerBtn.addEventListener("click", toggleMenu);
 
+// Event listener for window resize
+window.addEventListener("resize", function () {
     if (window.innerWidth > 768) {
         // This is the breakpoint set in your CSS media query
         nav.classList.remove("mobile-active"); // Hide the mobile menu
+        hamburgerIcon.textContent = "≡"; // reset to hamburger icon
     }
 });
 
-// Assuming these are related to the modal:
-btn.onclick = function () {
-  modal.style.display = "block";
-};
+// Function to scroll to a section when a navigation link is clicked
+function scrollToSection(targetId) {
+  const targetSection = document.getElementById(targetId);
 
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (targetSection) {
+      const yOffset = targetSection.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo({ top: yOffset, behavior: "smooth" });
+      toggleMenu(); // Close the mobile menu after clicking a link
   }
-};
+}
+
+// Event listener for navigation links
+const navLinks = document.querySelectorAll(".navbar a");
+navLinks.forEach(link => {
+    link.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent default link behavior
+        const targetId = link.getAttribute("data-link");
+        scrollToSection(targetId);
+    });
+});
+
+// Scroll to top button functionality
+const scrollToTopButton = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", function () {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+});
+
+scrollToTopButton.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
